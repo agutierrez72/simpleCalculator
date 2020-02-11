b8715974c3c7df8bfaa7c75ea6d266d7ea652f91
Angelo Kyrilov <akyrilov@ucmerced.edu>
2020-02-11 09:36:39 -0800

    Implemented echo endpoint

--- src/app.cpp
+++ src/app.cpp
@@ -1,4 +1,5 @@
 #include <server.h>
+#include <string>
 
 using namespace ucm;
 
@@ -8,6 +9,19 @@ int main(int argc, char** argv){
 
     server.route("/sayHello", [&](const request& req, response& res){
         res.sendHTML("Hello World");
+    });
+
+    server.route("/echo", [&](const request& req, response& res){
+        if (req.has_params({"text"})){
+            // Valid input
+            std::string text = req.url_params.get("text");
+
+            res.sendHTML(text);
+        }
+        else {
+            // User has not provided a value for text
+            res.sendError400();
+        }
     });
 
     server.run();
