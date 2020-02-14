1723879e5a7c92ac646b7c6283103cc9b1308785
agutierrez72 <agutierrez72@ucmerced.edu>
2020-02-13 17:19:39 -0800

    create functional add button using JSON

--- static/main.js
+++ static/main.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
+    $("#add").click(function(){
+        var lhs = $("#x").val();
+        var rhs = $("#y").val();
 
+        $.get("/add", {"x": lhs, "y": rhs}, function(response)){
+            var data = JSON.parse(response);
+
+            var ans = data["ans"];
+
+            $("#ans").html(ans);
+        }
+    });
 });
